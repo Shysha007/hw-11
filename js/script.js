16c28dccd40a5a9671a24077bdbcf6b7a818d107
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-10-27 01:51:23 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function exp(num, degree = 1){
+    if( num == false || isNaN(num))   return alert(`some error`);
+    if (degree == false || isNaN(degree))  return alert(`some error`); 
+    return alert(Math.pow(num, degree));
+}
+exp(+prompt(`Введите число`), +prompt('Введите степень'));
+
+
