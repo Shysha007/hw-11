95ffde94439da514d0c1d4b82d7e7f888f13e615
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-10-27 15:28:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 function exp(num, degree = 1){
-    if( num == false || isNaN(num))   return alert(`some error`);
-    if (degree == false || isNaN(degree))  return alert(`some error`); 
-    return alert(Math.pow(num, degree));
+    if(typeof(+num) !== `number` || isNaN(+num) || num === null || num.trim() === ``)   return alert(`some error`);
+    if(typeof(+degree) !== `number` || isNaN(+degree) || degree === null || degree.trim() === ``)   return alert(`some error`);
+    return alert(Math.pow(+num, +degree));
 }
-exp(+prompt(`Введите число`), +prompt(`Введите степень`));
+exp(prompt(`Введите число`),prompt(`Введите степень`));
+
 
 
